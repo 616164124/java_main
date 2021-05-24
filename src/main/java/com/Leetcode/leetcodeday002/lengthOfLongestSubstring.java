b5e4371a43bcc76771d8b1a1a0ff808aeb7b75a2
616164124@qq.com <ert123147>
2021-05-25 01:54:16 +0800

    2021年5月25日

--- src/main/java/com/Leetcode/leetcodeday002/lengthOfLongestSubstring.java
+++ src/main/java/com/Leetcode/leetcodeday002/lengthOfLongestSubstring.java
@@ -22,35 +22,35 @@ import java.util.Set;
  */
 public class lengthOfLongestSubstring {
 
-  public int lengthOfLongestSubstring(String s) {
-      int n = s.length();
-      int ans = 0;
-      for (int i = 0; i < n; i++) {
-          for (int j = i + 1; j <= n; j++) {
-              if (allUnique(s, i, j)) {
-                  ans = Math.max(ans, j - i);
-              }
-          }
-      }
-      return ans;
-  }
-
-  // 将每个字符都传人set（set 不重复集合）中
-  public boolean allUnique(String s, int start, int end) {
-    Set<Character> set = new HashSet<>();
-    for (int i = start; i < end; i++) {
-      Character ch = s.charAt(i);
-      if (set.contains(ch)) return false;
-      set.add(ch);
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (allUnique(s, i, j)) {
+                    ans = Math.max(ans, j - i);
+                }
+            }
+        }
+        return ans;
     }
-    return true;
-  }
 
-  public static void main(String[] args) {
-    String s = "abcedff";
-    lengthOfLongestSubstring lengthOfLongestSubstring = new lengthOfLongestSubstring();
+    // 将每个字符都传人set（set 不重复集合）中
+    public boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<Character>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
 
-    int i = lengthOfLongestSubstring.lengthOfLongestSubstring(s);
-    System.out.println(i);
-  }
+    public static void main(String[] args) {
+        String s = "abcedff";
+        lengthOfLongestSubstring lengthOfLongestSubstring = new lengthOfLongestSubstring();
+
+        int i = lengthOfLongestSubstring.lengthOfLongestSubstring(s);
+        System.out.println(i);
+    }
 }
