4a8dbbd496e28da2e8260978ffc62b795a6e20e3
616164124@qq.com <ert123147>
2021-06-27 15:34:28 +0800

    2021年6月27日

--- src/main/java/com/test/test001.java
+++ src/main/java/com/test/test001.java
@@ -3,7 +3,17 @@ package com.test;
 
 public class test001 {
     public static void main(String[] args) {
+        String sss="123|TAX";
+        System.out.println(sss);
 
+        boolean contains = sss.contains("|TAX");
+        System.out.println(contains);
+
+        String[] split = sss.split("\\|");
+        System.out.println(split[0]);
+        if(1==1)
+        System.out.println("1==1");
+        System.out.println("1111");
 
     }
 
