7c7e9ca74978e35e7073b9e10240bf55ff4b1ed1
616164124@qq.com <ert123147>
2021-07-13 15:55:41 +0800

    2021年7月13日

--- src/main/java/com/test/StringSplitTest.java
+++ src/main/java/com/test/StringSplitTest.java
@@ -1,9 +1,14 @@
 package com.test;
 
+import java.math.BigDecimal;
+
 public class StringSplitTest {
     public static void main(String[] args) {
         String pzbh = "e01408caeadd4d14a05de9ce1cfb0be9|TAX";
         String[] split = pzbh.split("\\|");
-        System.out.println(split[0]);
+        String s = "85162.8465";
+        String s1 = new BigDecimal(s).setScale(2, BigDecimal.ROUND_HALF_UP).toString();
+        double v = Double.parseDouble(s1);
+        System.out.println(v);
     }
 }
