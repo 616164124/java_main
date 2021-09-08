c131a849d5674c56e442f08f894d052572b2d24d
616164124@qq.com <ert123147>
2021-09-08 18:31:23 +0800

    2021年9月8日18:31:19

--- src/main/java/com/测试/test01.java
+++ src/main/java/com/测试/test01.java
@@ -2,6 +2,21 @@ package com.测试;
 
 public class test01 {
     public static void main(String[] args) {
+
+        int add = add();
+        System.out.println(add);
+
         System.out.println("jfklsjflsf");
+    }
+
+    public static int add(){
+        int i =0;
+        for (int i1 = 0; i1 < 10; i1++) {
+            i++;
+            if(i==7){
+                return i;
+            }
+        }
+        return i;
     }
 }
