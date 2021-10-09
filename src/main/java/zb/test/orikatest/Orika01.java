3c447277e5b4186aee73ed24487344fd27ffc6d0
616164124@qq.com <616164124@qq.com>
2021-10-09 18:34:03 +0800

    2021年10月9日18:33:59

--- src/main/java/zb/test/orikatest/Orika01.java
+++ src/main/java/zb/test/orikatest/Orika01.java
@@ -4,20 +4,22 @@ import com.dao.User;
 import com.dao.UserVO;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class Orika01 {
-
+    private static final Logger logger = LoggerFactory.getLogger(Orika01.class);
     public static void main(String[] args) {
 
         /**
          * 简单用法
          * 将user对象，复制给uservo（浅拷贝）
          */
-        User user = new User(12, "mu",12);
+        User user = new User(12, "mu", 12);
         MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
         UserVO map = mapperFactory.getMapperFacade().map(user, UserVO.class);
-        System.out.println("user对象："+user);
+        System.out.println("user对象：" + user);
         System.out.println("orika复制对象:" + map);
 
     }
---
+++ src/main/java/zb/算法/SingleNumber.java
@@ -0,0 +1,13 @@
+package zb.算法;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] nums = {1, 211, 22, 3, 23, 54};
+        int a=0;
+        for (int i = 0; i < 10; i++) {
+
+        }
+        System.out.println(a);
+        return;
+    }
+}
