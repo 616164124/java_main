d586e007c5a46df69ce451a88f54f7e086a0ffcc
hua <616164124@qq.com>
2021-10-20 01:14:23 +0800

    2021年10月20日01:14:14

--- src/main/java/com/test/RateTest.java
+++ src/main/java/com/test/RateTest.java
@@ -4,6 +4,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
+/**
+ * UUID(8-4-4-16)由以下几部分的组合：
+ *
+ * <p>　　（1）当前日期和时间，UUID的第一个部分与时间有关，如果你在生成一个UUID之后，过几秒又生成一个UUID，则第一个部分不同，其余相同。
+ *
+ * <p>　　（2）时钟序列
+ *
+ * <p>　　（3）全局唯一的IEEE机器识别号，如果有网卡，从网卡MAC地址获得，没有网卡以其他方式获得
+ */
 public class RateTest {
   public static void main(String[] args) {
     //
@@ -28,5 +37,6 @@ public class RateTest {
       money = (int) (money * rate);
       System.out.println(count + "\t" + simpleDateFormat.format(current) + "\t" + money);
     }
+
   }
 }
---
+++ src/main/java/zb/算法/位运算符的用法/找出单独的一个数.java
@@ -0,0 +1,19 @@
+package zb.算法.位运算符的用法;
+
+/**
+ * @author mikael
+ *
+ */
+public class 找出单独的一个数 {
+  public static void main(String[] args) {
+    //数组
+     int[] a = {1,1,2,2,23,42153,23};
+     int x = a[0];
+    for (int i = 0; i < a.length-1; i++) {
+      x= (x^a[i+1]);
+    }
+    System.out.println(x);
+    int c = 13^13;
+    System.out.println(c);
+  }
+}
