c34fabc7d1bfea89b20d174f81238818c2f750d8
616164124@qq.com <616164124@qq.com>
2021-11-16 16:31:05 +0800

    2021年11月9日18:35:04

--- src/main/java/zb/算法/test/Test01.java
+++ src/main/java/zb/算法/test/Test01.java
@@ -1,21 +1,53 @@
 package zb.算法.test;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 import java.util.PriorityQueue;
 
-/** @author mikael */
+/**
+ * @author mikael
+ */
 public class Test01 {
-  public static void main(String[] args) {
-    //
-    PriorityQueue<Integer> queue = new PriorityQueue<>();
-    queue.add(11);
-    queue.add(7);
-    queue.add(5);
-    queue.add(12);
-    queue.add(2);
-    queue.add(6);
-    queue.add(14);
-    while (!queue.isEmpty()) {
-      System.out.println(queue.poll());
+    public static void main(String[] args) {
+        //
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(11);
+        queue.add(7);
+        queue.add(5);
+        queue.add(12);
+        queue.add(2);
+        queue.add(6);
+        queue.add(14);
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
+        boolean subsequence = isSubsequence("34", "1234");
+        System.out.println(subsequence);
+        String a = "loeeeng";
+        String b = "neg";
+        System.out.println(a.indexOf(b, 2));
+
+        int i = a.indexOf(b);
+        System.out.println(i);
+        int aa=12,bb=12,cc=13;
+        int u= aa^cc^bb;
+        int uu=~aa;
+        System.out.println(uu);
+        int i1 = Integer.bitCount(1);
+
+        System.out.println(i1);
+        String r = Integer.toString(9, 2);
+
+
+
     }
-  }
+
+    public static boolean isSubsequence(String s, String target) {
+        for (int i = 0, pos = 0; i < s.length(); i++, pos++) {
+            pos = target.indexOf(s.charAt(i), pos);
+            if (pos == -1) return false;
+        }
+        return true;
+    }
+
 }
--- src/main/java/zb/算法/简单算法/希尔排序.java
+++ src/main/java/zb/算法/简单算法/希尔排序.java
@@ -3,6 +3,11 @@ package zb.算法.简单算法;
 import java.util.Arrays;
 
 public class 希尔排序 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(shellSort(new int[]{11, 21, 7, 9, 5, 8})));
+       System.out.println(Arrays.toString(test(new int[]{11, 21, 7, 9, 5, 8})));
+
+    }
 
     private static int[] shellSort(int[] arr) {
         int n = arr.length;
@@ -20,7 +25,23 @@ public class 希尔排序 {
         return arr;
     }
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(shellSort(new int[]{11, 21, 7, 9, 5, 8})));
+    public static int[] test(int[] arr){
+        int n = arr.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int key = arr[i];
+                int j = i;
+                while (j >= gap && arr[j - gap] > key) {
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+                arr[j] = key;
+            }
+        }
+
+        return arr;
     }
+
+
+
 }
