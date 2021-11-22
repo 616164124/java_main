2416f33b136f102c14d498e20a8fb13d0e194a34
616164124@qq.com <616164124@qq.com>
2021-11-22 17:43:49 +0800

    2021年11月22日17:43:46

--- src/main/java/zb/算法/简单算法/归并排序.java
+++ src/main/java/zb/算法/简单算法/归并排序.java
@@ -11,29 +11,30 @@ public class 归并排序 {
         System.out.println(Arrays.toString(nums));
         sort(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
+
     }
 
-    public static int[] sort(int[] nums, int low, int high) {
-        int mid = (low + high) / 2;
-        if (low < high) {
+    public static int[] sort(int[] nums, int left, int right) {
+        int mid = (left + right) / 2;
+        if (left < right) {
             // 左边
-            sort(nums, low, mid);
+            sort(nums, left, mid);
             // 右边
-            sort(nums, mid + 1, high);
+            sort(nums, mid + 1, right);
             // 左右归并
-            merge(nums, low, mid, high);
+            merge(nums, left, mid, right);
         }
         return nums;
     }
 
-    public static void merge(int[] nums, int low, int mid, int high) {
-        int[] temp = new int[high - low + 1];
-        int i = low;// 左指针
+    public static void merge(int[] nums, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left;// 左指针
         int j = mid + 1;// 右指针
         int k = 0;
 
         // 把较小的数先移到新数组中
-        while (i <= mid && j <= high) {
+        while (i <= mid && j <= right) {
             if (nums[i] <= nums[j]) {
                 temp[k++] = nums[i++];
             } else {
@@ -47,13 +48,13 @@ public class 归并排序 {
         }
 
         // 把右边边剩余的数移入数组
-        while (j <= high) {
+        while (j <= right) {
             temp[k++] = nums[j++];
         }
 
         // 把新数组中的数覆盖nums数组
         for (int k2 = 0; k2 < temp.length; k2++) {
-            nums[k2 + low] = temp[k2];
+            nums[k2 + left] = temp[k2];
         }
     }
 
