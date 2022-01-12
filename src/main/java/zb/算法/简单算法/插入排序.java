070354e27c3aff97a28b2ec7cc7242826c56470b
hua <616164124@qq.com>
2022-01-13 01:15:33 +0800

    2022年1月13日01:11:58

--- src/main/java/zb/算法/简单算法/插入排序.java
+++ src/main/java/zb/算法/简单算法/插入排序.java
@@ -49,5 +49,16 @@ public class 插入排序 {
         }
     }
 
+    public static  void test04(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i-1; j >=0&& arr[j]>arr[j+1] ; j--) {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+
 
 }
