3120fb4833635ab9f17b60080840f68862d7e47e
hua <616164124@qq.com>
2022-01-14 03:29:59 +0800

    2022年1月13日01:11:58

---
+++ src/main/java/zb/test/FindIp.java
@@ -0,0 +1,40 @@
+package zb.test;
+
+
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.model.CityResponse;
+import com.maxmind.geoip2.record.*;
+
+import java.io.File;
+import java.net.InetAddress;
+
+public class FindIp {
+    public static void main(String[] args) throws Exception {
+        File database = new File("src/main/java/zb/test/GeoLite2-City.mmdb");
+        DatabaseReader reader = new DatabaseReader.Builder(database).build();
+        // 指定获取到的IP地址
+        InetAddress ipAddress = InetAddress.getByName("115.192.186.180");
+        // 根据ip获取请求
+        CityResponse response = reader.city(ipAddress);
+        // 获取国家
+        Country country = response.getCountry();
+        // 获取编码
+        System.out.println(country.getIsoCode());
+        System.out.println(country.getName());
+        System.out.println();
+        System.out.println(country.getNames().get("zh-CN"));
+
+        Subdivision subdivision = response.getMostSpecificSubdivision();
+        System.out.println(subdivision.getName());
+        System.out.println(subdivision.getIsoCode());
+
+        City city = response.getCity();
+        System.out.println(city.getName());
+
+        Postal postal = response.getPostal();
+        System.out.println(postal.getCode());
+        Location location = response.getLocation();
+        System.out.println(location.getLatitude());
+        System.out.println(location.getLongitude());
+    }
+}
